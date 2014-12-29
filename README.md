9f933aead93f9a9fe653c7ce0a2958d21d1076b7
Patrick Wong <patrickwong@Patbookpro.local>
2014-12-29 00:04:03 -0800

    Add readme file

---
+++ README.md
@@ -0,0 +1,18 @@
+Tipsy
+=====
+
+This is a Tip Calculator for iOS, built using Swift & Xcode storyboards, for the Codepath prework assignment.
+
+Time spent: ~15 hours (Designing, following tutorial, debugging issues, spent a lot of time attempting things beyond the scope of the tutorial)
+
+Completed Stories:
+- Video Tutorial functionality (Segmented control, dynamic total)
+
+Optional Stories completed:
+- Custom colors, fonts, icons, launch screen, images
+- Animation block (currently fading in items when bill amount is input)
+
+Extras:
+- Included UIStepper to represent # of people, formula for total includes UIStepper value
+
+<img src="https://dl.dropboxusercontent.com/u/66196264/tipsy-screencap.gif" width="320">
--- Tipsy/ViewController.swift
+++ Tipsy/ViewController.swift
@@ -10,16 +10,92 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var peopleLabel: UILabel!
+    @IBOutlet weak var peopleStepper: UIStepper!
+    @IBOutlet weak var peopleImage: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        billField.text = "$"
+        tipLabel.text = "$"
+        totalLabel.text = "$0.00"
+        peopleLabel.text = "1"
+        
+        // Initial value for animation when no bill amount input
+        tipLabel.alpha = 0
+        tipControl.alpha = 0
+        peopleImage.alpha = 0
+        peopleLabel.alpha = 0
+        peopleStepper.alpha = 0
+        totalLabel.alpha = 0
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
 
+    @IBAction func onEditingChange(sender: AnyObject) {
+        var billAmount = NSString(string: billField.text).doubleValue
+        var tipPercentages = [0.15, 0.18, 0.20]
+        var tipPercentage = tipPercentages[tipControl.selectedSegmentIndex]
+        var tip = billAmount * tipPercentage
+        var people = peopleStepper.value
+        var total = (billAmount + tip)/people
+        
+        tipLabel.text = "$\(tip)"
+        totalLabel.text = "$\(total)"
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+        
+        // Fade in when bill amount input
+        UIView.animateWithDuration(0.2, animations: {
+            self.tipLabel.alpha = 1
+            self.tipControl.alpha = 1
+        })
+        
+        UIView.animateWithDuration(0.8, animations: {
+            self.peopleImage.alpha = 1
+            self.peopleLabel.alpha = 1
+            self.peopleStepper.alpha = 1
+            self.totalLabel.alpha = 1
+        })
+        
+        // Fade out when no bill amount
+        if(billField.text.isEmpty){
+            UIView.animateWithDuration(0.2, animations: {
+                self.tipLabel.alpha = 0
+                self.totalLabel.alpha = 0
+            })
+        }
+    }
+
+    @IBAction func valueChanged(sender: AnyObject) {
+        var billAmount = NSString(string: billField.text).doubleValue
+        var tipPercentages = [0.15, 0.18, 0.20]
+        var tipPercentage = tipPercentages[tipControl.selectedSegmentIndex]
+        var tip = billAmount * tipPercentage
+        var people = peopleStepper.value
+        var total = (billAmount + tip)/people
+        
+        tipLabel.text = "$\(tip)"
+        totalLabel.text = "$\(total)"
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+        peopleLabel.text = "\(Int(peopleStepper.value))"
+    }
+    
+    @IBAction func onTap(sender: AnyObject) {
+        view.endEditing(true)
+    }
 }
+
 
